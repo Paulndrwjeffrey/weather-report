978037c9919634abf0a0256d0a9d279ef68e497d
pablo <paulandrewjeffrey@gmail.com>
2021-03-25 18:05:04 -0700

    hmm

--- assets/script.js
+++ assets/script.js
@@ -9,8 +9,6 @@ function firstThing() {
     if (localStorage.recentSearches) {
         recentSearches = JSON.parse(localStorage.getItem('recentSearches'))
         createSearchList()
-        
-
     } else {
         recentSearches = []
     }
@@ -38,7 +36,7 @@ function search() {
     .then(response => response.json())
     .then(function(data){
         document.getElementById('citydate').innerText = data.name + '  ' + moment().format('l')
-        document.getElementById('duck').src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon +'@2x.png'
+        document.getElementById('duck').src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon +'@2x.png'
         duck.style.display = 'block'
         document.getElementById('temp').innerText = 'Temperature: ' + Math.round(data.main.temp) + ' Degrees'
         document.getElementById('humidity').innerText = 'Humidity: ' + data.main.humidity + '%'
@@ -46,7 +44,7 @@ function search() {
 
         var lat = data.coord.lat
         var lon = data.coord.lon 
-        fetch('http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=feca628886b7c4f2dff8cb9c14d6efb6')
+        fetch('https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=feca628886b7c4f2dff8cb9c14d6efb6')
         .then(response => response.json())
         .then(function(data){
             document.getElementById('UVindex').innerText = 'UV Index: ' + data.value
@@ -63,27 +61,27 @@ function search() {
                 .then(function(data) {
                     console.log(data)
                     document.getElementById('dayonedate').innerText = moment().add(1, 'days').format('l')
-                    document.getElementById('dayoneicon').src = 'http://openweathermap.org/img/wn/' + data.list[0].weather[0].icon +'@2x.png'
+                    document.getElementById('dayoneicon').src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon +'@2x.png'
                     document.getElementById('dayonetemp').innerText = 'Temp: ' + Math.round(data.list[0].main.temp) + 'deg.'
                     document.getElementById('dayonehum').innerText = 'Humidity: ' + data.list[0].main.humidity + '%'
 
                     document.getElementById('daytwodate').innerText = moment().add(2, 'days').format('l')
-                    document.getElementById('daytwoicon').src = 'http://openweathermap.org/img/wn/' + data.list[8].weather[0].icon +'@2x.png'
+                    document.getElementById('daytwoicon').src = 'https://openweathermap.org/img/wn/' + data.list[8].weather[0].icon +'@2x.png'
                     document.getElementById('daytwotemp').innerText = 'Temp: ' + Math.round(data.list[8].main.temp) + 'deg.'
                     document.getElementById('daytwohum').innerText = 'Humidity: ' + data.list[8].main.humidity + '%'
 
                     document.getElementById('daythreedate').innerText = moment().add(3, 'days').format('l')
-                    document.getElementById('daythreeicon').src = 'http://openweathermap.org/img/wn/' + data.list[16].weather[0].icon +'@2x.png'
+                    document.getElementById('daythreeicon').src = 'https://openweathermap.org/img/wn/' + data.list[16].weather[0].icon +'@2x.png'
                     document.getElementById('daythreetemp').innerText = 'Temp: ' + Math.round(data.list[16].main.temp) + 'deg.'
                     document.getElementById('daythreehum').innerText = 'Humidity: ' + data.list[16].main.humidity + '%'
 
                     document.getElementById('dayfourdate').innerText = moment().add(4, 'days').format('l')
-                    document.getElementById('dayfouricon').src = 'http://openweathermap.org/img/wn/' + data.list[24].weather[0].icon +'@2x.png'
+                    document.getElementById('dayfouricon').src = 'https://openweathermap.org/img/wn/' + data.list[24].weather[0].icon +'@2x.png'
                     document.getElementById('dayfourtemp').innerText = 'Temp: ' + Math.round(data.list[24].main.temp) + 'deg.'
                     document.getElementById('dayfourhum').innerText = 'Humidity: ' + data.list[24].main.humidity + '%'
 
                     document.getElementById('dayfivedate').innerText = moment().add(5, 'days').format('l')
-                    document.getElementById('dayfiveicon').src = 'http://openweathermap.org/img/wn/' + data.list[32].weather[0].icon +'@2x.png'
+                    document.getElementById('dayfiveicon').src = 'https://openweathermap.org/img/wn/' + data.list[32].weather[0].icon +'@2x.png'
                     document.getElementById('dayfivetemp').innerText = 'Temp: ' + Math.round(data.list[32].main.temp) + 'deg.'
                     document.getElementById('dayfivehum').innerText = 'Humidity: ' + data.list[32].main.humidity + '%'
                 })
